43acdedac5a30880602a19760d94bf4be0584c76
Carson Farmer <59753859+cfarm6@users.noreply.github.com>
2023-07-07 20:17:09 -0400

    Working test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using LinearAlgebra
 
 @testset "ContinuumMechanicsBase.jl" begin
     F = I(3)
-    @test I₁(F) = 3
-    @test I₂(F) = 3
-    @test I₃(F) = 1
-    @test J(F)= 1
-    @test_throws
+    @test I₁(F) == 3
+    @test I₂(F) == 3
+    @test I₃(F) == 1
+    @test J(F) == 1
+
 end
