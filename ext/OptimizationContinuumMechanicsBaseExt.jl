b58a506c48dbca3338e476de58197805527f6a62
Joby M. Anthony III <jmanthony1@liberty.edu>
2025-03-12 08:38:07 -0400

    empty function for `OptimizationProblem`

--- ext/OptimizationContinuumMechanicsBaseExt.jl
+++ ext/OptimizationContinuumMechanicsBaseExt.jl
@@ -50,6 +50,21 @@ function parameter_bounds(
     return (lb = lb, ub = ub)
 end
 
+"""
+$(SIGNATURES)
+
+Creates an `OptimizationProblem` for use in [`Optimization.jl`](https://docs.sciml.ai/Optimization/stable/) to find the optimal parameters.
+
+# Arguments:
+- `ψ`: material model to use
+- `test` or `tests`: A single or vector of `::AbstractMaterialTest`s to use when fitting the parameters
+- `u₀`: Initial guess for parameters
+- `ps`: Any additional parameters for calling `predict()`
+- `adb`: Select differentiation type from [`ADTypes.jl`](https://github.com/SciML/ADTypes.jl). The type is automatically applied to the type of AD applied to the `OptimizationProblem` also.
+- `loss`: Loss function from [`LossFunctions.jl`](https://github.com/JuliaML/LossFunctions.jl)
+"""
+function MaterialOptimizationProblem end
+
 
 
 end # end of module
